d6487bc9b67923a0e868c042ef9046ed60d4cb75
lil-Ribhav-Bhatt1012 <77582313+lil-Ribhav-Bhatt1012@users.noreply.github.com>
2023-05-17 18:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ const outputValue = document.getElementById('outputValue');
 const convertBtn = document.getElementById('convertBtn');
 
 const unitFactors = {
-  meters: 1,
-  kilometers: 0.001,
-  centimeters: 100,
-  millimeters: 1000,
-  miles: 0.000621371,
-  inches: 39.3701,
-  feet: 3.28084,
+  meters: 100,
+  kilometers: 100000,
+  centimeters: 1,
+  millimeters: 0.1,
+  miles: 160934.4,
+  inches: 2.54,
+  feet: 30.48,
   grams: 1,
-  kilograms: 0.001,
-  pounds: 0.00220462,
-  ounces: 0.035274,
+  kilograms: 1000,
+  pounds: 453.592,
+  ounces: 28.3495,
   liters: 1,
-  milliliters: 1000,
-  gallons: 0.264172,
-  celsius: 1,
-  fahrenheit: 1,
-  kelvin: 1
+  milliliters: 0.001,
+  gallons: 3785.41,
 };
+
+
 
 function convertTemperature(value, fromUnit, toUnit) {
   let celsiusValue;
